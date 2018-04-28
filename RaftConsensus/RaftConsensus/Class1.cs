fb7014a9f534736fbabc09996e783725261bd537
Joshua and Sean <joshua+sean-pair@codedev.com.au>
2018-04-28 21:05:40 +1000

    Created blank repo for project

---
+++ RaftConsensus/RaftConsensus/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamDecided.Raft
+{
+    class Class1
+    {
+    }
+}
---
+++ RaftConsensus/RaftConsensusTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RaftConsensusTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RaftConsensusTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b779a06b-7291-4f80-9bb0-90aa2a534125")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
