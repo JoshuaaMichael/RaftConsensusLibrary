a343574d5de19beb82a79ebdf9b5dd6942b15393
Joshua and Sean <joshua+sean-pair@codedev.com.au>
2018-05-02 21:40:14 +1000

    Merged all the project into one Visual Studio solution. Updated the name of the RaftLogging in the class diagram.

---
+++ RaftConsensus/RaftLogging/RaftLogging.cs
@@ -0,0 +1,12 @@
+﻿namespace TeamDecided.RaftLogging
+{
+    public interface IRaftLogging
+    {
+        void Trace(string format, params object[] args);
+        void Debug(string format, params object[] args);
+        void Info(string format, params object[] args);
+        void Warn(string format, params object[] args);
+        void Error(string format, params object[] args);
+        void Fatal(string format, params object[] args);
+    }
+}
