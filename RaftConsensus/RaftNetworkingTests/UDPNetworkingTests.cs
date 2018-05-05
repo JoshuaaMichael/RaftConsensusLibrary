c12c1a3e45f7c38031546e8354b9144449a8cb4e
sean <13326738+neas21@users.noreply.github.com>
2018-05-05 12:12:42 +1000

    Working on UDPNetwork Intergration tests.

---
+++ RaftConsensus/RaftNetworkingTests/UDPNetworkingTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+//using NUnit.Mocks;
+using TeamDecided.RaftNetworking;
+using TeamDecided.RaftNetworking.Interfaces;
+using TeamDecided.RaftNetworking.Messages;
+
+namespace TeamDecided.RaftNetworking.Tests
+{
+    [TestFixture]
+    public class UDPNetworkingTests
+    {
+        
+        private string to;
+        private string from;
+        private string stringData;
+        private byte[] byteData;
+
+        private StringMessage stringMessage;
+        private ByteMessage byteMessage;
+
+        static Random rand = new Random();
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            to = new Guid().ToString();
+            from = new Guid().ToString();
+            stringData = new Guid().ToString();
+            byteData = new byte[256];
+
+            stringMessage = new StringMessage(to, from, stringData);
+            byteMessage = new ByteMessage(to, from, byteData);
+
+        }
+
+        [Test]
+        public void IT_StartSendDispose_()
+        {
+            IUDPNetworking sut = new UDPNetworking();
+            Assert.DoesNotThrow(() => { sut.Start(5554); });
+            Assert.DoesNotThrow(() => { sut.SendMessage(stringMessage);  });
+            Assert.IsNotNull(sut);
+            sut.Dispose();
+            Assert.IsNull(sut);
+        }
+    }
+}
