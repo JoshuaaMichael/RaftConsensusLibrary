1c355b0263a06f3ac71718fc15889085b8a2a9e4
sean <13326738+neas21@users.noreply.github.com>
2018-05-05 14:58:23 +1000

    Test Update

--- RaftConsensus/RaftNetworkingTests/UDPNetworkingTests.cs
+++ RaftConsensus/RaftNetworkingTests/UDPNetworkingTests.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Threading;
 using NUnit.Framework;
-//using NUnit.Mocks;
-using TeamDecided.RaftNetworking;
 using TeamDecided.RaftNetworking.Interfaces;
 using TeamDecided.RaftNetworking.Messages;
 
@@ -21,28 +21,45 @@ namespace TeamDecided.RaftNetworking.Tests
 
         static Random rand = new Random();
 
+        private const string IP_TO_BIND = "127.0.0.1";
+        private const int SUT_PORT = 5554;
+
+        BaseMessage recievedMessage;
+        ManualResetEvent onRecieveMessage = new ManualResetEvent(false);
+
         [SetUp]
         public void BeforeTest()
         {
-            to = new Guid().ToString();
-            from = new Guid().ToString();
-            stringData = new Guid().ToString();
+            to = Guid.NewGuid().ToString();
+            from = Guid.NewGuid().ToString();
+            stringData = Guid.NewGuid().ToString();
             byteData = new byte[256];
 
             stringMessage = new StringMessage(to, from, stringData);
             byteMessage = new ByteMessage(to, from, byteData);
-
         }
 
         [Test]
-        public void IT_StartSendDispose_()
+        public void IT_StartSendReceiveDispose_()
         {
             IUDPNetworking sut = new UDPNetworking();
-            Assert.DoesNotThrow(() => { sut.Start(5554); });
-            Assert.DoesNotThrow(() => { sut.SendMessage(stringMessage);  });
-            Assert.IsNotNull(sut);
+            sut.ManualAddPeer(to, new IPEndPoint(IPAddress.Parse(IP_TO_BIND), SUT_PORT));
+            sut.OnMessageReceived += Sut_OnMessageReceived;
+
+            Assert.DoesNotThrow(() => { sut.Start(SUT_PORT); });
+            Assert.DoesNotThrow(() => { sut.SendMessage(stringMessage); });
+
+            //onRecieveMessage.WaitOne();
+            //You've got a message, deal with it and check it's the same
+
             sut.Dispose();
-            Assert.IsNull(sut);
+            Assert.True(sut.IsDisposed());
+        }
+
+        private void Sut_OnMessageReceived(object sender, BaseMessage e)
+        {
+            recievedMessage = e;
+            onRecieveMessage.Set();
         }
     }
 }
