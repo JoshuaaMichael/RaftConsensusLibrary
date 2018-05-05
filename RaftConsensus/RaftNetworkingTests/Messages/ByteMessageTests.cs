3b6de99b88f601dadc4d55fcbba19a259ff82ed7
Joshua and Sean <joshua+sean-pair@codedev.com.au>
2018-05-05 10:31:47 +1000

    Removed unnecessary using statements. Copied and changed the tests over to suite the ByteMessage class.

--- RaftConsensus/RaftNetworkingTests/Messages/ByteMessageTests.cs
+++ RaftConsensus/RaftNetworkingTests/Messages/ByteMessageTests.cs
@@ -1,25 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
+using TeamDecided.RaftNetworking.Messages;
 
 namespace TeamDecided.RaftNetworking.Tests.Messages
 {
     [TestFixture]
     class ByteMessageTests
     {
+        string to;
+        string from;
+        byte[] data;
+
+        ByteMessage sut;
+
+        static Random rand = new Random();
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            to = new Guid().ToString();
+            from = new Guid().ToString();
+            byte[] dataArray = new byte[256];
+            rand.NextBytes(dataArray);
+            data = dataArray;
+
+            sut = new ByteMessage(to, from, data);
+        }
+
         [Test]
-        //IT_UseCase_ExpectedBehviour
-        //UT_MethodName_StateUnderTest_ExpectedBehavior
         public void IT_SerialiseDeserialise_AllMembersAreEqual()
         {
             //Arrange
 
             //Act
+            byte[] serialise = sut.Serialize();
+            ByteMessage deserialised = BaseMessage.Deserialize<ByteMessage>(serialise);
 
             //Assert
+            Assert.AreEqual(to, deserialised.To);
+            Assert.AreEqual(from, deserialised.From);
+            Assert.AreEqual(typeof(ByteMessage), deserialised.MessageType);
+            Assert.AreEqual(data, deserialised.Data);
+        }
+
+        [Test]
+        public void UT_GetTo_MemberIsEqual()
+        {
+            Assert.AreEqual(to, sut.To);
+        }
+
+        [Test]
+        public void UT_GetFrom_MemberIsEqual()
+        {
+            Assert.AreEqual(from, sut.From);
+        }
+
+        [Test]
+        public void UT_GetMessageType_MemberIsEqual()
+        {
+            Assert.AreEqual(typeof(ByteMessage), sut.MessageType);
+        }
+
+        [Test]
+        public void UT_GetData_MemberIsEqual()
+        {
+            Assert.AreEqual(data, sut.Data);
+        }
+
+        [Test]
+        public void UT_Serialize_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => { sut.Serialize(); });
+        }
+
+        [Test]
+        public void UT_Serialize_ReturnsByteArrayWithData()
+        {
+            byte[] serialized = sut.Serialize();
+            Assert.IsNotNull(serialized);
+            Assert.IsTrue(serialized.Length > 0);
+        }
+
+        [Test]
+        public void UT_Deserialize_DoesNotThrow()
+        {
+            byte[] serialized = sut.Serialize();
+
+            Assert.DoesNotThrow(() => { BaseMessage.Deserialize<ByteMessage>(serialized); });
+        }
+
+        [Test]
+        public void UT_Deserialize_ReturnsStringWithData()
+        {
+            byte[] serialized = sut.Serialize();
+            ByteMessage deserialized = BaseMessage.Deserialize<ByteMessage>(serialized);
+
+            Assert.IsNotNull(deserialized);
+            Assert.IsTrue(deserialized.Data.Length > 0);
         }
     }
 }
