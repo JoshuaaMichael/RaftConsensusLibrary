bbd4b22505dc5153a5bd8c6b4a5b91c1a2353ce4
sean <13326738+neas21@users.noreply.github.com>
2018-05-12 11:36:33 +1000

    Stubbed out RaftNodeInfo Class and compiled basic unit tests for class.

---
+++ RaftConsensus/RaftConsensus/RaftNodeInfo/RaftNodeInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamDecided.RaftConsensus
+{
+    public class NodeInfo
+    {
+        public string NodeName { get; private set; }
+        public int NextIndex { get; set; }
+        public int MatchIndex { get; set; }
+        public bool VoteGranted { get; set; }
+        public DateTime LastReceived { get; private set; }
+        public DateTime RPCDue { get; set; }
+
+        public NodeInfo(string nodeName)
+        {
+            this.NodeName = nodeName;
+            LastReceived = DateTime.Now;
+        }
+
+        public void UpdateLastReceived()
+        {
+            LastReceived = DateTime.Now;
+        }
+
+    }
+}
