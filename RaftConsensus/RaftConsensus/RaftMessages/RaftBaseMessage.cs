f2f5a0dc5ce7eee536ce6f9d15839d2774164cb4
Joshua and Sean <joshua+sean-pair@codedev.com.au>
2018-06-07 22:21:05 +1000

    Added GUIDs to messages to make tracing in debug logs easier

--- RaftConsensus/RaftConsensus/RaftMessages/RaftBaseMessage.cs
+++ RaftConsensus/RaftConsensus/RaftMessages/RaftBaseMessage.cs
@@ -1,19 +1,25 @@
-﻿using TeamDecided.RaftNetworking.Messages;
+﻿using System;
+using TeamDecided.RaftNetworking.Messages;
 
 namespace TeamDecided.RaftConsensus.RaftMessages
 {
     public class RaftBaseMessage : BaseMessage
     {
         public string ClusterName { get; set; }
+        public string MessageGuid { get; set; }
 
         public RaftBaseMessage() { }
 
         public RaftBaseMessage(string to, string from, string clusterName)
-            : base(to, from) { ClusterName = clusterName; }
+            : base(to, from)
+        {
+            ClusterName = clusterName;
+            MessageGuid = Guid.NewGuid().ToString().Substring(24); //We only want the last set
+        }
 
         public override string ToString()
         {
-            return string.Format("Message Contents: Type:{0}, To: {1}, From: {2}", MessageType, To, From);
+            return string.Format("Message Contents: Type:{0}, GUID: {1}, To: {2}, From: {3}", MessageType, MessageGuid, To, From);
         }
     }
 }
