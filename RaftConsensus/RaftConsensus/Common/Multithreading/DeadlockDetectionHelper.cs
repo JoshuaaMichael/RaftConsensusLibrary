5caba66da493ad4e6247b0effdd25ff7eb1ca26c
Joshua and Sean <joshua+sean-pair@codedev.com.au>
2018-06-17 00:41:47 +1000

    It was actually 50 lines saved! I didn't get the unneeded using statements D:

---
+++ RaftConsensus/RaftConsensus/Common/Multithreading/DeadlockDetectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamDecided.RaftConsensus.Common.Multithreading
+{
+    internal class DeadlockDetectionHelper<T> where T : IComparable, IFormattable, IConvertible
+    {
+        private object _classLockObject;
+        private object[] _lockObjects;
+        private HashSet<int> _lockObjectLookup;
+
+        public DeadlockDetectionHelper()
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("Genertic type must be a type of enum");
+            }
+        }
+    }
+}
