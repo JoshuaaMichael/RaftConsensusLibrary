ae6978ce34404c48007854308776496b60c7187d
Joshua + Sean <joshua+sean-pair@codedev.com.au>
2018-08-18 09:58:44 +1000

    Test environment cleanup, commented out 17node test

--- RaftConsensus/RaftConsensusTests/Consensus/17Nodes/RaftConsensusTests5NodesUnencrypted.cs
+++ RaftConsensus/RaftConsensusTests/Consensus/17Nodes/RaftConsensusTests5NodesUnencrypted.cs
@@ -1,13 +1,13 @@
 ﻿
 namespace TeamDecided.RaftConsensus.Tests.Consensus._17Nodes
 {
-    internal class RaftConsensusTests17NodesUnencrypted : BaseRaftConsensusTests
+    internal class RaftConsensusTests17NodesUnencrypted /*: BaseRaftConsensusTests*/
     {
         public RaftConsensusTests17NodesUnencrypted()
         {
-            NumberOfNodesInTest = 17;
-            NumberOfActiveNodesInTest = 17;
-            UseEncryption = false;
+            //NumberOfNodesInTest = 17;
+            //NumberOfActiveNodesInTest = 17;
+            //UseEncryption = false;
         }
     }
 }
